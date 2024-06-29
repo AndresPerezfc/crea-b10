b52411c7b7b202e8cfdc20aa27dd117a9873cc6f
andresperezfc <felipe3apc@gmail.com>
2024-06-28 22:18:16 -0500

    Cambio de idioma menor

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
@@ -56,7 +56,7 @@ export const ChapterVideoForm = ({
           {!isEditing && !initialData.videoUrl && (
             <>
               <PlusCircle className="h-4 w-4 mr-2" />
-              Add a video
+              Añadir video
             </>
           )}
 
