50d4ed582d6750621f409f65bf0a7a28f0d9d3fe
andresperezfc <felipe3apc@gmail.com>
2024-07-01 21:46:12 -0500

    Breve cambio de idioma en chapter video form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-video-form.tsx
@@ -49,7 +49,7 @@ export const ChapterVideoForm = ({
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
-        Chapter video
+        Video de la lección
         <Button onClick={toggleEdit} variant="ghost">
           {isEditing && <>Cancel</>}
 
